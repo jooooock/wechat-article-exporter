d4585a8315318c324cbafbef14cc95c2ef872c9e
jooooock <findsource@proton.me>
2024-07-24 23:05:43 +0800

    批量下载

--- components/BatchDownloadButton.vue
+++ components/BatchDownloadButton.vue
@@ -4,7 +4,11 @@
       :disabled="loading"
       class="bg-sky-900 hover:bg-sky-700 disabled:bg-sky-900 min-w-24 focus:outline-none focus:ring-2 focus:ring-sky-400 focus:ring-offset-2 focus:ring-offset-sky-50 text-white font-semibold h-10 px-4 rounded flex items-center justify-center">
     <Loader v-if="loading" :size="20" class="animate-spin"/>
-    <span v-if="loading">{{page}}: {{articles.length}}</span>
+    <span v-if="loading">{{phase}}:
+      <span v-if="phase === '抓取文章链接'">{{articles.length}}/{{page}}</span>
+      <span v-if="phase === '下载文章内容'">{{downloadedArticles.length}}/{{articles.length}}</span>
+      <span v-if="phase === '打包'">/{{downloadedArticles.length}}</span>
+    </span>
     <span v-else>批量下载</span>
   </button>
 </template>
@@ -13,19 +17,28 @@
 import {Loader} from "lucide-vue-next";
 import type {AppMsgEx, AppMsgPublishResponse, PublishInfo, PublishPage} from "~/types/types";
 import {sleep} from "@antfu/utils";
+import JSZip from "jszip";
+import {saveAs} from "file-saver";
+import mime from "mime";
 
+type AppMsgExWithHTML = AppMsgEx & {
+  html?: string
+};
 
 const token = useToken()
 const activeAccount = useActiveAccount()
 
 const page = ref(1)
-const articles: AppMsgEx[] = reactive([])
+const articles: AppMsgExWithHTML[] = reactive([])
 const loading = ref(false)
+const phase = ref()
+const downloadedArticles = computed(() => articles.filter(article => !!article.html))
 
 async function batchDownload() {
   loading.value = true
 
   try {
+    phase.value = '抓取文章链接'
     let data = []
     do {
       data = await getArticleList(page.value)
@@ -33,12 +46,38 @@ async function batchDownload() {
       await sleep(5000)
       page.value++
     } while (data.length > 0)
+
+    console.log(articles)
   } catch (e: any) {
-    alert(e.message)
+    console.warn(e.message)
   }
 
+
+  phase.value = '下载文章内容'
+  for (const article of articles.filter(article => !article.is_deleted)) {
+    try {
+      article.html = await downloadHTML(article)
+      await sleep(3000)
+    } catch (e: any) {
+      console.warn(e.message)
+    }
+  }
+
+  phase.value = '打包'
+  const zip = new JSZip()
+  for (const article of downloadedArticles.value) {
+    try {
+      await packArticle(article, zip.folder(article.title)!)
+      await sleep(2000)
+    } catch (e: any) {
+      console.warn(e.message)
+    }
+  }
+
+  const blob = await zip.generateAsync({type: 'blob'})
+  saveAs(blob, 'results.zip')
+
   loading.value = false
-  console.log(articles)
 }
 
 async function getArticleList(page = 1) {
@@ -68,6 +107,97 @@ async function getArticleList(page = 1) {
     throw new Error('session expired')
   } else {
     throw new Error(resp.base_resp.err_msg)
+  }
+}
+
+async function downloadHTML(article: AppMsgEx) {
+  const fullHTML = await $fetch<string>('/api/download?url=' + encodeURIComponent(article.link))
+
+  const parser = new DOMParser()
+  const document = parser.parseFromString(fullHTML, 'text/html')
+  const $pageContent = document.querySelector('#page-content')
+  if (!$pageContent) {
+    throw new Error('下载失败，请重试')
+  }
+  return $pageContent.outerHTML
+}
+
+async function packArticle(article: AppMsgExWithHTML, zip: JSZip) {
+  try {
+    const parser = new DOMParser()
+    const html = article.html!
+    const $pageContent = parser.parseFromString(article.html!, 'text/html')
+    $pageContent.querySelector('#js_content')?.removeAttribute('style')
+
+
+    const imgs = $pageContent.querySelectorAll('img')
+    zip.folder('assets')
+    for (const img of imgs) {
+      const imgData = await $fetch<Blob>(img.src)
+      const uuid = new Date().getTime() + Math.random().toString()
+      const ext = mime.getExtension(imgData.type)
+      zip.file(`assets/${uuid}.${ext}`, imgData)
+
+      // 重写路径，指向本地图片文件
+      img.src = `./assets/${uuid}.${ext}`
+    }
+
+    // 处理背景图片
+    const map = new Map<string, string>()
+    let pageContentHTML = article.html!
+
+    // 收集背景图片
+    const bgImages = new Set<string>()
+    pageContentHTML.replaceAll(/((?:background|background-image): url\((?:&quot;)?)((?:https?|\/\/)[^)]+?)((?:&quot;)?\))/gs, (match, p1, url, p3) => {
+      bgImages.add(url)
+      return `${p1}${url}${p3}`
+    })
+    for (const url of bgImages) {
+      const imgData = await $fetch<Blob>(url)
+      const uuid = new Date().getTime() + Math.random().toString()
+      const ext = mime.getExtension(imgData.type)
+
+      zip.file(`assets/${uuid}.${ext}`, imgData)
+      map.set(url, `assets/${uuid}.${ext}`)
+    }
+
+    pageContentHTML = pageContentHTML.replaceAll(/((?:background|background-image): url\((?:&quot;)?)((?:https?|\/\/)[^)]+?)((?:&quot;)?\))/gs, (match, p1, url, p3) => {
+      if (map.has(url)) {
+        const path = map.get(url)!
+        return `${p1}./${path}${p3}`
+      } else {
+        console.warn('背景图片丢失')
+        return `${p1}${url}${p3}`
+      }
+    })
+
+
+    const indexHTML = `<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="Content-Type" content="text/html; charset=utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport"
+          content="width=device-width,initial-scale=1.0,maximum-scale=1.0,user-scalable=0,viewport-fit=cover">
+    <style>
+#page-content {
+max-width: 667px;
+margin: 0 auto;
+}
+img {
+max-width: 100%;
+}
+    </style>
+</head>
+<body>
+${pageContentHTML}
+</body>
+</html>`
+
+    zip.file('index.html', indexHTML)
+  } finally {
+
   }
 }
 </script>
