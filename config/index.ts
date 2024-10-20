0a4784614804b6200d8f4e4acb6f39e7b4141eed
jooooock <findsource@proton.me>
2024-10-20 22:12:23 +0800

    01和02号代理已失效

--- config/index.ts
+++ config/index.ts
@@ -21,8 +21,8 @@ export const ACCOUNT_TYPE: Record<number, string> = {
  * 代理池
  */
 export const AVAILABLE_PROXY_LIST: string[] = [
-    'https://vproxy-01.deno.dev/',
-    'https://vproxy-02.deno.dev/',
+    // 'https://vproxy-01.deno.dev/',
+    // 'https://vproxy-02.deno.dev/',
     'https://vproxy-03.deno.dev/',
     'https://vproxy-04.deno.dev/',
     'https://vproxy-05.deno.dev/',
